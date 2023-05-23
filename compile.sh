f290982a71e9dd2fdb3f430750a6948f245b2bd4
rusefillc <sdfsdfqsf2334234234>
2023-05-23 17:24:57 -0400

    nicer progress

--- compile.sh
+++ compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export BOARD_DIR=../../..
-
 # todo: remove 'atlas' part once we have https://github.com/rusefi/rusefi/issues/5284
 cd ext/rusefi/firmware/config/boards/atlas
 
-bash ../common_make.sh X ARCH_STM32F4
+bash ../common_make.sh ../../../../.. ARCH_STM32F4
