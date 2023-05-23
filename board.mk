329a978a8f66cb404e1c0146d21d20fd977ad8da
rusefillc <sdfsdfqsf2334234234>
2023-05-23 17:23:22 -0400

    nasty progress

--- board.mk
+++ board.mk
@@ -1,1 +1,4 @@
+DDEFS += -DFIRMWARE_ID=\"Paralela\"
+DDEFS += -DSHORT_BOARD_NAME=paralela
+
 
--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export BOARD_DIR=../../..
+
 # todo: remove 'atlas' part once we have https://github.com/rusefi/rusefi/issues/5284
 cd ext/rusefi/firmware/config/boards/atlas
-bash ../common_make.sh paralela
+
+bash ../common_make.sh X ARCH_STM32F4
